d3b8db9c39eb0d20d6fc2bb60f615387e479e535
Gorgamol <Gorgamol@users.noreply.github.com>
2017-12-13 00:50:53 +0100

    12.12.17

--- Aufgabe 5/main.c
+++ Aufgabe 5/main.c
@@ -5,14 +5,16 @@ extern unsigned short addsub(int op1, int op2, int what, int *result)
 
 int main(int argc, char** argv) {
 
+	//Korrekte Parameteranzahl?
 	if(argc != 4) {
 		printf("\nUngueltige Parameteranzahl!");
 		printf("\nNutzung: flagtest [op1] [+/-] [op2]");
 		return -1;
 	}
 
-	int what = 1;
+	int what = 1; //1 = '+' / 2 = '-'
 
+	//Korrekte Rechenoperation?
 	if(*argv[2] == '-') {
 		what = 2;
 	} else if(*argv[2] != '+') {
@@ -27,11 +29,13 @@ int main(int argc, char** argv) {
 	unsigned short flags = addsub(op1, op2, what, &result);
 	int flagArray[16];
 
+	//flagArray auffüllen: index 0 = Carry Flag, dann SBC-86 Reihenfolge
 	for(int i = 0; i < 16; i++) {
 		flagArray[i] = flags % 2;
 		flags = flags / 2;
 	}
 
+	//Ausgabe flags (nur SBC-86 Reihenfolge)
 	printf("\nFlags:\nO D I T S Z  A  P  C\n");
 	for(int i = 11; i < 11; i++) {
 		printf("%d ", flagArray[i]);
@@ -52,7 +56,6 @@ int main(int argc, char** argv) {
 	} else {
 		printf(" (Ergebnis ist falsch!)");
 	}
-
 
 	return 0;
 }
