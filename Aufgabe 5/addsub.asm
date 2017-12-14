1bca821f71d70576d3efa9337e2dc86634f85c52
Gorgamol <Gorgamol@users.noreply.github.com>
2017-12-14 22:44:14 +0100

    14.12.17

--- Aufgabe 5/addsub.asm
+++ Aufgabe 5/addsub.asm
@@ -2,26 +2,26 @@ section .text
 
 global addsub
 
-sub equ 02h		; C Programm setzt "what" = 2, wenn argv[2] == '-'
+subt equ 02h		; C Programm setzt "what" = 2, wenn argv[2] == '-'
 
 addsub:
 	push ebp		; Stackframe => Basepointer sichern
 	mov ebp, esp		; Basepointer = Stackpointer (ebp als fester ankerpunkt für übergebene funktionsargumente)
 
-	mov eax, [ebp+4]	; op1 vom Stack in eax
-	mov ebx, [ebp+12]	; what (+/-) vom Stack in ebx
-	cmp ebx, sub		; Prüfe ob Subtraktion
+	mov eax, [ebp+8]	; op1 vom Stack in eax
+	mov ebx, [ebp+16]	; what (+/-) vom Stack in ebx
+	cmp ebx, subt		; Prüfe ob Subtraktion
 	je subtraktion		; Springe zu subtraktion, wenn gleich (jump equal)
 
-	add ax, [ebp+8]		; op1 + op2 (16 bit addition)
+	add ax, [ebp+12]		; op1 + op2 (16 bit addition)
 	jmp return		; Überpringe subtraktion
 
 subtraktion:
-	sub ax, [ebp+8]		; op1 + op2 (16 bit subtraktion)
+	sub ax, [ebp+12]		; op1 + op2 (16 bit subtraktion)
 
 return:
-	mov ebx, [ebp+16]	; Adresse von übergabeparameter ergebnis in ebx
-	mov [ebx], eax		; Ergebnis aus eax über die Adresse zurückgeben
+	mov ebx, [ebp+20]	; Adresse von übergabeparameter ergebnis in ebx
+	mov [ebx], ax		; Ergebnis aus eax über die Adresse zurückgeben
 
 	pushfd			; 32 Bit EFLAGS Register auf den Stack schieben
 	pop eax			; Die 32 oberen Bits (EFLAGS Register) vom Stack in eax (EAX Register = Rückgabe an C Programm)
--- Aufgabe 5/main.c
+++ Aufgabe 5/main.c
@@ -4,7 +4,7 @@
 extern unsigned short addsub(int op1, int op2, int what, int *result);
 
 int main(int argc, char** argv) {
-
+	
 	//Korrekte Parameteranzahl?
 	if(argc != 4) {
 		printf("\nUngueltige Parameteranzahl!");
@@ -24,33 +24,34 @@ int main(int argc, char** argv) {
 	}
 
 	short op1 = atoi(argv[1]);
-	short op2 = atoi(argv[2]);
+	short op2 = atoi(argv[3]);
 	short result = 0;
 	unsigned short flags = addsub(op1, op2, what, &result);
 	int flagArray[16];
+	int i;
 
 	//flagArray auffüllen: index 0 = Carry Flag, dann SBC-86 Reihenfolge
-	for(int i = 0; i < 16; i++) {
+	for(i = 0; i < 16; i++) {
 		flagArray[i] = flags % 2;
 		flags = flags / 2;
 	}
 
 	//Ausgabe flags (nur SBC-86 Reihenfolge)
-	printf("\nFlags:\nO D I T S Z  A  P  C\n");
-	for(int i = 11; i < 11; i++) {
+	printf("\nFlags:\nO D I T S Z   A   P   C\n");
+	for(i = 11; i >= 0; i--) {
 		printf("%d ", flagArray[i]);
 	}
 
-	printf("\nErgebnis und Operanden Signed:");
-	printf("%d %c %d = %d", op1, op2, *argv[2], result);
+	printf("\n\nErgebnis und Operanden Signed: ");
+	printf("%d %c %d = %d", op1, *argv[2], op2, result);
 	if(flagArray[11] == 0) {	//overflow flag gesetzt? Ja = falsch, Nein = richtig
 		printf(" (Ergebnis ist richtig!)\n");
 	} else {
 		printf(" (Ergebnis ist falsch!)\n");
 	}
 
-	printf("\nErgebnis und Operanden Unsigned:");
-	printf("%d %c %d = %d", (unsigned short) op1, (unsigned short) op2, *argv[2], (unsigned short) result);
+	printf("\nErgebnis und Operanden Unsigned: ");
+	printf("%d %c %d = %d", (unsigned short) op1, *argv[2], (unsigned short) op2, (unsigned short) result);
 	if(flagArray[0] == 0) {		//carry flag gesetzt? Ja = falsch, Nein = richtig
 		printf(" (Ergebnis ist richtig!)\n");
 	} else {
